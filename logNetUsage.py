4d91e28e488d042b11f925ef839e21ba8769f0cd
d-wizard <dw.wizard@gmail.com>
2022-04-10 19:58:02 -0500

    Add script that will periodically update the Net Usage log file.

---
+++ logNetUsage.py
@@ -0,0 +1,11 @@
+import getNetUsageChartArray
+import time
+
+while (1):
+   try:
+      netUsageTx, netUsageRx = getNetUsageChartArray.getNetUsage()
+      if netUsageTx != None and netUsageRx != None:
+         getNetUsageChartArray.updateNetUsageLogFile(netUsageTx, netUsageRx)
+   except:
+      pass
+   time.sleep(10)
